d343cb202117608364ff0120a53a7a8dd9c5314c
Marc Clifton <marc.clifton@gmail.com>
2017-08-31 14:13:53 -0400

    removed zencoders' implementation from repo.

--- Clifton.Kademlia/UnitTests/Properties/AssemblyInfo.cs
+++ Clifton.Kademlia/UnitTests/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("efa0342e-15f2-4d83-8d0b-edae32275500")]
+[assembly: Guid("173c551b-01a8-4f3f-aa73-e9ae4ed77d18")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
