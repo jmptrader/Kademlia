78628f59dfe7a748e74c50f380472d485a4c866d
cliftonm <marc.clifton@gmail.com>
2017-09-13 12:17:34 -0400

    Distribution tests.

--- Clifton.Kademlia/UnitTests2/AddContactTests.cs
+++ Clifton.Kademlia/UnitTests2/AddContactTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography;
 using System.Linq;
+using System.Numerics;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Clifton.Kademlia;
@@ -11,6 +14,8 @@ namespace UnitTests2
 	[TestClass]
 	public class AddContactTests
 	{
+		public const string CRLF = "\r\n";
+
 		[TestMethod]
 		public void UniqueIDAddTest()
 		{
@@ -46,22 +51,28 @@ namespace UnitTests2
         [TestMethod]
         public void ForceFailedAddTest()
         {
-            // force host node ID to < 2^159
+            // force host node ID to < 2^159 so the node ID is not in the 2^159 ... 2^160 range
             byte[] hostID = new byte[20];
-            hostID[19] = 0x7F;      
+            hostID[19] = 0x7F;
             BucketList bucketList = new BucketList(new ID(hostID));
 
             // Also add a contact in this 0 - 2^159 range, arbitrarily something not our host ID.
+			// This ensures that only one bucket split will occur after 20 nodes with ID >= 2^159 are added,
+			// otherwise, buckets will in the 2^159 ... 2^160 space.
             byte[] id = new byte[20];
             id[0] = 1;
             bucketList.AddContact(new Contact(null, new ID(id)));
 
-            // make sure contact ID's all have the same 5 bit prefix and are in the 2^159 through 2^160 - 1 space
+            // make sure contact ID's all have the same 5 bit prefix and are in the 2^159 ... 2^160 - 1 space
             byte[] contactID = new byte[20];
             contactID[19] = 0x80;
-            // 1000 0xxx prefix, xxx starts at 100 (4)
-            // this ensures that all the contacts in a bucket match only the prefix.
-            byte shifter = 0x4;
+            // 1000 xxxx prefix, xxxx starts at 1000 (8)
+            // this ensures that all the contacts in a bucket match only the prefix as only the first 5 bits are shared.
+			// |----| shared range
+			// 1000 1000 ...
+			// 1000 1100 ...
+			// 1000 1110 ...
+            byte shifter = 0x08;
             int pos = 19;
 
             Constants.K.ForEach(() =>
@@ -78,31 +89,18 @@ namespace UnitTests2
             });
 
             Assert.IsTrue(bucketList.Buckets.Count == 2, "Bucket split should not have occurred.");
-            Assert.IsTrue(bucketList.Buckets[0].Contacts.Count == 1, "Expected 1 contact.");
-            Assert.IsTrue(bucketList.Buckets[1].Contacts.Count == 20, "Expected 20 contacts.");
+            Assert.IsTrue(bucketList.Buckets[0].Contacts.Count == 1, "Expected 1 contact in bucket 0.");
+            Assert.IsTrue(bucketList.Buckets[1].Contacts.Count == 20, "Expected 20 contacts in bucket 1.");
 
-            // This next contact should not split the bucket as depth == 5.
-            contactID[pos] |= shifter;
-            bucketList.AddContact(new Contact(null, new ID(contactID)));
-            Assert.IsTrue(bucketList.Buckets.Count == 1, "Bucket split should not have occurred.");
-            Assert.IsTrue(bucketList.Buckets[0].Contacts.Count == 20, "Expected 20 contacts.");
-        }
+			// This next contact should not split the bucket as depth == 5 and therefore adding the contact will fail.
+			// Any unique ID >= 2^159 will do.
+			id = new byte[20];
+			id[19] = 0x80;
+			bucketList.AddContact(new Contact(null, new ID(id)));
 
-        [TestMethod]
-		public void RandomPrefixDistributionTest()
-		{
-			BucketList bucketList = new BucketList(ID.RandomID);
-			List<int> contactsAdded = new List<int>();
-
-			100.ForEach(() =>
-			{
-				3200.ForEach(() => bucketList.AddContact(new Contact(null, ID.RandomID)));
-				int contacts = bucketList.Buckets.Sum(b => b.Contacts.Count);
-				contactsAdded.Add(contacts);
-			});
-
-			Assert.IsTrue(contactsAdded.Average().ApproximatelyEquals(3300, 100), "Unexpected distribution.");
-			Assert.IsTrue(contactsAdded.Select(n => (double)n).StdDev().ApproximatelyEquals(630, 60), "Bad distribution");
+			Assert.IsTrue(bucketList.Buckets.Count == 2, "Bucket split should not have occurred.");
+			Assert.IsTrue(bucketList.Buckets[0].Contacts.Count == 1, "Expected 1 contact in bucket 0.");
+			Assert.IsTrue(bucketList.Buckets[1].Contacts.Count == 20, "Expected 20 contacts in bucket 1.");
 		}
 
 		[TestMethod]
@@ -132,21 +130,62 @@ namespace UnitTests2
 		}
 
 		[TestMethod]
-		public void DistributionTest3()
+		public void RandomPrefixDistributionTest()
 		{
-			SHA1Managed sha1 = new SHA1Managed();
-			Random rnd = new Random();
-			byte[] buffer = new byte[20];
-			rnd.NextBytes(buffer);
-			BucketList bucketList = new BucketList(new ID(sha1.ComputeHash(buffer)));
+			List<int> contactsAdded = new List<int>();
 
-			3200.ForEach(() =>
+			100.ForEach(() =>
 			{
-				rnd.NextBytes(buffer);
-				bucketList.AddContact(new Contact(null, new ID(sha1.ComputeHash(buffer))));
+				BucketList bucketList = new BucketList(ID.RandomID);
+				3200.ForEach(() => bucketList.AddContact(new Contact(null, ID.RandomID)));
+				int contacts = bucketList.Buckets.Sum(b => b.Contacts.Count);
+				contactsAdded.Add(contacts);
 			});
 
-			int contacts = bucketList.Buckets.Sum(b => b.Contacts.Count);
+			double avg = contactsAdded.Average();
+			double stdev = contactsAdded.Select(n => (double)n).StdDev();
+			Assert.IsTrue(contactsAdded.Average().ApproximatelyEquals(1900, 200), "Unexpected distribution: avg = " + avg);
+			Assert.IsTrue(stdev.ApproximatelyEquals(750, 50), "Bad distribution: stdev = " + stdev);
+		}
+
+		[TestMethod]
+		public void DistributionTestForEachPrefix()
+		{
+			Random rnd = new Random();
+			StringBuilder sb = new StringBuilder();
+			byte[] buffer = new byte[20];
+
+			160.ForEach((i) =>
+			{
+				BucketList bucketList = new BucketList(new ID(BigInteger.Pow(new BigInteger(2), i)));
+
+				3200.ForEach(() =>
+				{
+					rnd.NextBytes(buffer);
+					bucketList.AddContact(new Contact(null, new ID(buffer)));
+				});
+
+				int contacts = bucketList.Buckets.Sum(b => b.Contacts.Count);
+				sb.Append(i + "," + contacts + CRLF);
+			});
+
+			File.WriteAllText("prefixTest.txt", sb.ToString());
+		}
+
+		[TestMethod]
+		public void DistributionTestForEachPrefixWithRandomPrefixDistributedContacts()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			160.ForEach((i) =>
+			{
+				BucketList bucketList = new BucketList(new ID(BigInteger.Pow(new BigInteger(2), i)));
+				3200.ForEach(() => bucketList.AddContact(new Contact(null, ID.RandomID)));
+				int contacts = bucketList.Buckets.Sum(b => b.Contacts.Count);
+				sb.Append(i + "," + contacts + CRLF);
+			});
+
+			File.WriteAllText("prefixTest.txt", sb.ToString());
 		}
 	}
 }
